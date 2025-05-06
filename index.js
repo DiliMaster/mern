af062b8e863d1bb8d3bc70a4b8f756d054699f3c
Anjana Dilshan <dilaking97@gmail.com>
2025-05-06 23:55:26 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+import express from 'express';
+
+let app = express();
+
+// function successFullyStarted(){
+//    console.log('Sever is running on port 3000');
+//}
+
+
+app.listen(3000,successFullyStarted) 
